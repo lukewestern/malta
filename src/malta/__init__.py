92fe71a20f47f2e084fc28b78fca9bc166bf9033
lukewestern <luke.western@bristol.ac.uk>
2023-06-23 19:03:37 +0100

    Added test and updates readme

--- src/malta/__init__.py
+++ src/malta/__init__.py
@@ -1,3 +1,3 @@
 # read version from installed package
-from importlib.metadata import version
-__version__ = version("malta")
+#from importlib.metadata import version
+#__version__ = version("malta")
